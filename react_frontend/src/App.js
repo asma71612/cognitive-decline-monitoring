953a4f98bf4d355b85f7afab338f1e9a0b74778a
Asma <asma71612@gmail.com>
2025-01-21 22:44:39 -0500

    Creating support page for patients

--- react_frontend/src/App.js
+++ react_frontend/src/App.js
@@ -4,7 +4,7 @@ import HomeLandingPage from './pages/HomeLandingPage';
 import PatientLoginPage from './pages/patient/PatientLoginPage';
 import PatientHomePage from './pages/patient/PatientHomePage';
 import PatientReportingLandingPage from './pages/patient/PatientReportingLandingPage';
-import InfoPage from './pages/patient/InfoPage';
+import SupportPage from './pages/patient/SupportPage';
 import GeneralInstructionsPage from './pages/patient/GeneralInstructionsPage';
 import LightingCalibration from './pages/patient/LightingCalibration';
 import GazeCalibration from './pages/patient/GazeCalibration';
@@ -23,7 +23,7 @@ const App = () => {
         <Route path="/patient-login" element={<PatientLoginPage />} />
         <Route path="/patient-home-page" element={<PatientHomePage />} />
         <Route path="/patient-reporting-landing-page" element={<PatientReportingLandingPage />} />
-        <Route path="/info-page" element={<InfoPage />} />
+        <Route path="/support-page" element={<SupportPage />} />
         <Route path="/general-instructions" element={<GeneralInstructionsPage />} />
         <Route path="/lighting-calibration" element={<LightingCalibration />} />
         <Route path="/gaze-calibration" element={<GazeCalibration />} />
