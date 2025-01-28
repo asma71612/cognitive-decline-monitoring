fecf3d48572c6fbd61c2f65960871eb60cc8b54a
Asma <asma71612@gmail.com>
2025-01-28 00:16:06 -0500

    Create instructions modal component

--- react_frontend/src/pages/games/naturesGaze/NaturesGaze1Instructions.js
+++ react_frontend/src/pages/games/naturesGaze/NaturesGaze1Instructions.js
@@ -1,32 +1,24 @@
-import React from 'react';
-import { useNavigate } from "react-router-dom";
-import './NaturesGaze1Instructions.css';
-import naturesGazeBackgroundDark from '../../../assets/naturesGazeBackgroundDark.svg';
+import React from "react";
+import InstructionsPage from "../../../components/InstructionsModal";
+import naturesGazeBackgroundDark from "../../../assets/naturesGazeBackgroundDark.svg";
 
 const NaturesGaze1Instructions = () => {
-  const navigate = useNavigate();
   return (
-    <div className="instructions-page" style={{ backgroundImage: `url(${naturesGazeBackgroundDark})` }}>
-      <div className="game-instruction-card">
-        <h1>Nature's Gaze I</h1>
-        <h2>How to Play</h2>
-        <ol>
-          <li>You will begin by focusing on the red dot at the centre of the screen.</li>
-          <li>A bird will appear either horizontally or vertically from the central red dot.</li>
-        </ol>
-        <p>
-          <strong>Your task is as soon as the bird appears, to shift your gaze to follow the bird as fast and as accurately as possible.</strong>
-        </p>
-        <p>
-          In total, there will be <span className="placeholder">__</span> birds, each alternating their appearance horizontally or vertically from the central red dot. Continue to follow them as quickly and accurately as possible.
-        </p>
-        <p>
-          Note that between every bird, you will need to shift your gaze back to the central red dot.
-        </p>
-        <button className="start-button" onClick={() => navigate('/natures-gaze-1-game')}>Start</button>
-      </div>
-    </div>
+    <InstructionsPage
+      title="Nature's Gaze I"
+      howToPlay="How to Play"
+      instructionsList={[
+        "You will begin by focusing on the red dot at the centre of the screen.",
+        "A bird will appear either horizontally or vertically from the central red dot.",
+      ]}
+      strongText="Your task is as soon as the bird appears, to shift your gaze to follow the bird as fast and as accurately as possible."
+      subText="In total, there will be __ birds, each alternating their appearance horizontally or vertically from the central red dot. Continue to follow them as quickly and accurately as possible."
+      noteText="Note that between every bird, you will need to shift your gaze back to the central red dot."
+      startButtonText="Start"
+      startButtonRoute="/natures-gaze-1-game"
+      backgroundImage={naturesGazeBackgroundDark}
+    />
   );
-}
+};
 
 export default NaturesGaze1Instructions;
--- react_frontend/src/pages/games/naturesGaze/NaturesGaze2Instructions.js
+++ react_frontend/src/pages/games/naturesGaze/NaturesGaze2Instructions.js
@@ -1,54 +1,23 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
-import "./NaturesGaze2Instructions.css";
+import InstructionsPage from "../../../components/InstructionsModal";
 import naturesGazeBackgroundDark from "../../../assets/naturesGazeBackgroundDark.svg";
 
 const NaturesGaze2Instructions = () => {
-  const navigate = useNavigate();
   return (
-    <div
-      className="instructions-page"
-      style={{ backgroundImage: `url(${naturesGazeBackgroundDark})` }}
-    >
-      <div className="game-instruction-card">
-        <h1>Nature's Gaze II</h1>
-        <h2>How to Play</h2>
-        <ol>
-          <li>
-            You will begin by focusing on the red dot at the centre of the
-            screen.
-          </li>
-          <li>
-            A cat will appear either horizontally or vertically from the central
-            red dot.
-          </li>
-        </ol>
-        <p>
-          <strong>
-            Your task is as soon as the cat appears, to shift your gaze in the
-            opposite direction from where the dog appears as fast and as
-            accurately as possible. For example, if the cat appears to the left
-            of the central red dot, look right.
-          </strong>
-        </p>
-        <p>
-          In total, there will be <span className="placeholder">__</span> cats,
-          each alternating their appearance horizontally or vertically from the
-          central red dot. Continue to look in the opposite direction as quickly
-          and accurately as possible.
-        </p>
-        <p>
-          Note that between every cat, you will need to shift your gaze back to
-          the central red dot.
-        </p>
-        <button
-          className="start-button"
-          onClick={() => navigate("/natures-gaze-2-game")}
-        >
-          Start
-        </button>
-      </div>
-    </div>
+    <InstructionsPage
+      title="Nature's Gaze II"
+      howToPlay="How to Play"
+      instructionsList={[
+        "You will begin by focusing on the red dot at the centre of the screen.",
+        "A cat will appear either horizontally or vertically from the central red dot.",
+      ]}
+      strongText="Your task is as soon as the cat appears, to shift your gaze in the opposite direction from where the cat appears as fast and as accurately as possible. For example, if the cat appears to the left of the central red dot, look right."
+      subText="In total, there will be __ cats, each alternating their appearance horizontally or vertically from the central red dot. Continue to look in the opposite direction as quickly and accurately as possible."
+      noteText="Note that between every cat, you will need to shift your gaze back to the central red dot."
+      startButtonText="Start"
+      startButtonRoute="/natures-gaze-2-game"
+      backgroundImage={naturesGazeBackgroundDark}
+    />
   );
 };
 
