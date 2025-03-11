bb0d167094f9ec768b0444eca831b096ee4fde7d
amenasyed <80727252+amenasyed@users.noreply.github.com>
2025-03-11 12:36:22 -0400

    minor text changes to instructions

--- react_frontend/src/pages/games/processQuest/ProcessQuestInstructions.js
+++ react_frontend/src/pages/games/processQuest/ProcessQuestInstructions.js
@@ -10,9 +10,9 @@ const ProcessQuestInstructions = () => {
     <InstructionsPage
       title="Process Quest"
       howToPlay="How to Play"
-      firstText="A prompt will appear on the screen. Your task is to provide clear and organized verbal instructions in response to the prompt."
-      strongText="You have up to 15 minutes to complete your explanation. Aim to cover all the necessary details within this time."
-      italicsText="Note that your audio will be recorded and transcribed for further analysis."
+      firstText="A prompt will appear on the screen. Your task is to respond to it with clear and organized verbal instructions."
+      strongText="You have up to 15 minutes to complete your explanation."
+      italicsText="Note that for the duration of the task, your audio will be recorded and transcribed for further analysis. Ensure your microphone permissions are enabled on this website."
       startButtonText="Start"
       startButtonRoute={`/process-quest/${userId}`}
       backgroundImage={memoryVaultBackgroundDark}
