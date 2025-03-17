eccd01fa52dd3f1379dda903125c7d00601a9a6b
Sophia Lollino <slollino@uwaterloo.ca>
2025-03-17 17:32:06 -0400

    Revert "Merge branch 'gaze-calibration-new' of https://github.com/asma71612/cognitive-decline-monitoring into updating_reporting"
    This reverts commit c3d4c96b0ba4b4681e8df58328e486c4075425c0, reversing
    changes made to 40c253df4dbe2980c0c7ffff4c83f38a61a058a2.

--- react_frontend/src/pages/patient/GazeCalibration.js
+++ react_frontend/src/pages/patient/GazeCalibration.js
@@ -1,44 +1,17 @@
-import React, { useState, useEffect, useRef } from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import './GazeCalibration.css';
 
 const GazeCalibration = () => {
   const navigate = useNavigate();
-  const { userId } = useParams();
-  const [calibrationComplete, setCalibrationComplete] = useState(false);
-  const iframeRef = useRef(null);
-
-  // For future enhancement: We'll eventually move all logic from gaze_calibration.html here
-  // For now, we're using an iframe to embed the existing Flask implementation
-
-  // This function could potentially be used to resize the iframe if needed
-  const handleIframeLoad = () => {
-    if (iframeRef.current) {
-      // If we need to make any adjustments to the iframe after loading, we can do it here
-      console.log("Gaze calibration iframe loaded");
-    }
-  };
 
   return (
     <div className="gaze-calibration-container">
-      {/* <div className="header">
-        <h1>Gaze Calibration</h1>
-        <button className="back-button" onClick={() => navigate(-1)}>
-          ← Back
-        </button>
-      </div> */}
-      
-      <div className="calibration-frame-container">
-        <iframe 
-          ref={iframeRef}
-          src="http://localhost:5000/gaze-calibration-test" 
-          title="Gaze Calibration"
-          className="calibration-frame"
-          onLoad={handleIframeLoad}
-          scrolling="no"
-          allow="camera"
-        />
-      </div>
+      <h1>Gaze Calibration</h1>
+      <p>Follow the instructions to calibrate your gaze.</p>
+      <button className="next-button" onClick={() => navigate('/natures-gaze-1-instructions')}>
+        Next
+      </button>
     </div>
   );
 };
