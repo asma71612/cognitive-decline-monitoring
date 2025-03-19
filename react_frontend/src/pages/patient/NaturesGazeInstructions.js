05c4c6847adf6b49de2082193de7daf23f846ebe
Asma Ansari <asma.ansari@asma.ansari9303>
2025-03-19 15:33:01 -0400

    style clean up

--- react_frontend/src/pages/patient/NaturesGazeInstructions.js
+++ react_frontend/src/pages/patient/NaturesGazeInstructions.js
@@ -8,16 +8,15 @@ const NaturesGazeInstructions = () => {
 
   return (
     <InstructionsPage
-      title="Nature's Gaze - Saccade Task"
+      title="Nature's Gaze"
       howToPlay="How It Works"
-      firstText="This activity measures how quickly and accurately your eyes can respond to visual stimuli."
+      firstText="This activity measures how quickly and accurately your eyes can respond to a bird on the screen."
       instructionsList={[
-        "Focus on the bird at the center of the screen",
-        "When the bird moves to a new position, follow the instructions on screen",
-        "You may be asked to look TOWARD or AWAY from the bird",
-        "Try to respond as quickly and accurately as possible"
+        "Focus on the bird at the center of the screen.",
+        "When the bird moves to a new position, follow the instructions on screen.",
+        "Try to respond as quickly and accurately as possible."
       ]}
-      italicsText="The eye tracking technology will measure your eye movements throughout the test. Please remain seated in the same position and try not to move your head excessively."
+      italicsText="Please remain seated in the same position and try not to move your head excessively."
       startButtonText="Start Game"
       startButtonRoute={`/natures-gaze${userId ? `/${userId}` : ''}`}
       backgroundImage={naturesGazeBackground}
